5c679f6648b46dc59b689b4e0de2356b2f3b6c90
Caleb Brewer <calebbrewer@Calebs-MacBook-Pro.local>
2017-01-21 16:48:46 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,252 @@
+"use strict";
+var request = require('request');
+var toJson = require('safe-json-parse/callback');
+
+class DynamicsCrmRest {
+    constructor(params) {
+        this.params = params;
+    }
+
+    auth(params, callback) {
+        //Need error handling to make sure the required params are provided
+        var body = 'client_id=' + encodeURI(params.clientId) + '&resource=https%3A%2F%2F' + params.orgName + '.crm.dynamics.com&username=' + encodeURI(params.username) + '&password=' + encodeURI(params.password) + '&grant_type=password'
+
+        request({ method: 'POST', url: 'https://login.windows.net/' + params.orgDomain + '/oauth2/token', headers: { 'Cache-Control': 'no-cache', 'Content-Type': 'application/x-www-form-urlencoded' }, body: body }, function (err, res, body) {
+            if (err) {
+                callback(err)
+            }
+            else {
+                toJson(body, function (err, json) {
+                    if (err) {
+                        console.error('toJson error in auth: ' + err);
+                    }
+                    else {
+                        if (json.error) {
+                            callback(json.error);
+                        }
+                        else {
+                            callback(null, json.access_token);
+                        }
+                    }
+                });
+            }
+        });
+    };
+
+    get isTokenGood() {
+        //Need to renew the token
+        console.log(this.lastTokenCreated);
+        if (this.lastTokenCreationTime && new Date().getMilliseconds > this.lastTokenCreationTime + 3600) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
+
+    getToken(callback) {
+        /*if(this.isTokenGood) {
+            console.log('good token', this.lastTokenCreationTime);
+            callback(this.lastTokenCreated);
+        }
+        else {*/
+        //console.log('bad token');
+        var setTokenInfo = function (token, tokenSetTime) {
+            this.lastTokenCreated = token;
+            this.lastTokenCreationTime = tokenSetTime;
+        }
+
+        this.auth(this.params, function (err, authToken) {
+            if (err) {
+                console.error(err);
+                return 'error'
+            }
+            else {
+                //setTokenInfo(authToken, new Date().getMilliseconds);            
+                callback(authToken);
+            }
+        });
+        //}
+    };
+
+    /***************/
+
+    get(endPoint, callback) {
+        var orgName = this.params.orgName;
+
+        this.getToken(function (token) {
+            request({ url: 'https://' + orgName + '.api.crm.dynamics.com/api/data/v8.1/' + endPoint, headers: { 'OData-MaxVersion': '4.0', 'OData-Version': '4.0', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token } }, function (err, response, body) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    toJson(body, function (err, json) {
+                        if (err) {
+                            console.error('toJson error in auth: ' + err);
+                        }
+                        else {
+                            if (json.error) {
+                                callback(json.error);
+                            }
+                            else {
+                                if (json.value) {
+                                    callback(null, json.value);
+                                }
+                                else {
+                                    callback(null, json);
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    };
+
+    post(endPoint, data, callback) {
+        var orgName = this.params.orgName;
+
+        this.getToken(function (token) {
+            request({ method: 'POST', url: 'https://' + orgName + '.api.crm.dynamics.com/api/data/v8.1/' + endPoint, headers: { 'OData-MaxVersion': '4.0', 'OData-Version': '4.0', 'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token }, body: JSON.stringify(data) }, function (err, response, body) {
+                if (err) {
+                    callback(err);
+                }
+                else if (response.statusCode == 204) {
+                    //Expect this when your operation succeeds but does not return data in the response body.
+                    callback(null, 'Dynamics CRM POST success!')
+                }
+                else {
+                    toJson(body, function (err, json) {
+                        if (err) {
+                            console.error('toJson error in auth: ' + err);
+                        }
+                        else {
+                            if (json.error) {
+                                callback(json.error);
+                            }
+                            else {
+                                if (json.value) {
+                                    callback(null, json.value);
+                                }
+                                else {
+                                    callback(null, json);
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    };
+
+    put(endPoint, data, callback) {
+        var orgName = this.params.orgName;
+
+        this.getToken(function (token) {
+            request({ method: 'PUT', url: 'https://' + orgName + '.api.crm.dynamics.com/api/data/v8.1/' + endPoint, headers: { 'OData-MaxVersion': '4.0', 'OData-Version': '4.0', 'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token }, body: JSON.stringify(data) }, function (err, response, body) {
+                if (err) {
+                    callback('request error', err);
+                }
+                else if (response.statusCode == 204) {
+                    //Expect this when your operation succeeds but does not return data in the response body.
+                    callback(null, 'Dynamics CRM PUT success!')
+                }
+                else {
+                    toJson(body, function (err, json) {
+                        if (err) {
+                            console.error('toJson error in auth: ' + err);
+                        }
+                        else {
+                            if (json.error) {
+                                callback('json error: ', json);
+                            }
+                            else {
+                                if (json.value) {
+                                    callback(null, json.value);
+                                }
+                                else {
+                                    callback(null, json);
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    };
+
+    patch(endPoint, data, callback) {
+        var orgName = this.params.orgName;
+
+        this.getToken(function (token) {
+            request({ method: 'PATCH', url: 'https://' + orgName + '.api.crm.dynamics.com/api/data/v8.1/' + endPoint, headers: { 'OData-MaxVersion': '4.0', 'OData-Version': '4.0', 'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token }, body: JSON.stringify(data) }, function (err, response, body) {
+                if (err) {
+                    callback('request error', err);
+                }
+                else if (response.statusCode == 204) {
+                    //Expect this when your operation succeeds but does not return data in the response body.
+                    callback(null, 'Dynamics CRM PATCH success!')
+                }
+                else {
+                    toJson(body, function (err, json) {
+                        if (err) {
+                            console.error('toJson error in auth: ' + err);
+                        }
+                        else {
+                            if (json.error) {
+                                callback('json error: ', json);
+                            }
+                            else {
+                                if (json.value) {
+                                    callback(null, json.value);
+                                }
+                                else {
+                                    callback(null, json);
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    };
+
+    delete(endPoint, callback) {
+        var orgName = this.params.orgName;
+
+        this.getToken(function (token) {
+            request({ method: 'DELETE', url: 'https://' + orgName + '.api.crm.dynamics.com/api/data/v8.1/' + endPoint, headers: { 'OData-MaxVersion': '4.0', 'OData-Version': '4.0', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token } }, function (err, response, body) {
+                //console.log(response);
+                if (err) {
+                    callback('request error', err);
+                }
+                else if (response.statusCode == 204) {
+                    //Expect this when your operation succeeds but does not return data in the response body.
+                    callback(null, 'Dynamics CRM DELETE success!')
+                }
+                else {
+                    toJson(body, function (err, json) {
+                        if (err) {
+                            console.error('toJson error in auth: ' + err);
+                        }
+                        else {
+                            if (json.error) {
+                                callback(json);
+                            }
+                            else {
+                                if (json.value) {
+                                    callback(null, json.value);
+                                }
+                                else {
+                                    callback(null, json);
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        });
+    };
+};
+
+module.exports = DynamicsRest;
---
+++ readme.md
@@ -0,0 +1,51 @@
+dynamics-crm-rest is a node module for interfacing with the Microsoft Dynamics CRM REST API. If you’ve had issues with Microsoft’s adal-node tool for authing against the API (The auth is handled by an Azure app) then this module will make you happy.
+
+`GET, POST, PATCH, PUT, and DELETE are supported.` 
+
+**This module needs more error handling and tests/ That said it does work well.**
+
+##Install
+`nom install dynamics-crm-rest`
+
+##Setting Up Azure App for Authing
+Dynamics uses Azure AD Applications to get an OAuth token, so you will need to register a “Native” app with Azure and use the “Application ID” in dynamics-crm-rest. You will need to give the app rights to CRM and you will need an AD user with rights to the app. You will be using the users creds to auth against the app. I don’t like having to use an AD user, but that’s the way they do it. If adal-node worked I would use it.
+Here is a link to a Microsoft article showing how to register an app with Azure AD. Scroll down to “App registration for OAuth authentication”. Also, make sure you choose “Native App” when registering. [https://msdn.microsoft.com/en-us/library/mt622431.aspx](https://msdn.microsoft.com/en-us/library/mt622431.aspx).
+
+##Module Usage
+```
+let crm = new DynamicsRest({
+    orgName: ‘exampleOrgName’,
+    orgDomain: ‘example.onmicrosoft.com',
+    clientId: ‘’, //This is the app id from the Azure app you registered
+    username: '', //The Azure AD user with rights to the app
+    password: '' //The user’s password
+});
+
+crm.post("contacts", {
+    firstname: ‘John’,
+    lastname: ‘Smith’,
+    mobilephone: ‘555-555-5555’,
+    emailaddress1: ‘example@example.com’,
+}, function(err, res) {
+    if(err) {
+        console.error(err);
+    }
+    else {
+        console.log(res);
+    }
+});  
+
+crm.get("contacts", function(err, res) {
+    if(err) {
+        console.error(err);
+    }
+    else {
+        console.log(res);
+    }
+});
+```
+
+##To Do
+- Add better error handling
+- Add tests
+- Add token refresh so it doesn't have to get a new token every time. 
